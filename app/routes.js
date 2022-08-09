5bb9bb5a15b28cfc242019c532f0551b1cac8fff
victorivandfe <77013703+victorivandfe@users.noreply.github.com>
2022-08-09 14:50:59 +0100

    Updated to funding prototype

--- app/routes.js
+++ app/routes.js
@@ -4,3 +4,23 @@ const router = express.Router()
 // Add your routes here - above the module.exports line
 
 module.exports = router
+
+
+// Re-direct from multiple  radios 
+router.post('/sprint-0-beta/page1', function (req, res) {
+
+    let levytype = req.session.data['levytype']
+
+    console.log(req.session.data['levytype'])
+
+    //Option 1
+    if (levytype === 'Under £3 million') {
+        res.redirect('/sprint-0-beta/page1b')
+    }
+    //Option 2
+    else if (levytype === 'Over £3 million') {
+        res.redirect('/sprint-0-beta/page2')
+    }
+
+    //end
+})
